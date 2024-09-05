97ccec3159c0d4f241c0c78d1bc0196bd642dd3f
ahslxjvhapt12 <likjhee1@gmail.com>
2024-09-05 14:39:40 +0900

    StageSystem

--- Assets/01.Scripts/Inven/ItemSO.cs
+++ Assets/01.Scripts/Inven/ItemSO.cs
@@ -1,29 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public enum Type
 {
-    Mineral,
     Interectable,
+    Default,
     None
 }
 
 [CreateAssetMenu(fileName = "ItemSO")]
 public class ItemSO : ScriptableObject
 {
-    [SerializeField] string itemName;
-    public string ItemName => itemName;
-
-    [SerializeField] Sprite itemImage;
-    public Sprite ItemImage => itemImage;
-
-    [SerializeField] int stackCount;
-    public int StackCount => stackCount;
-
-    [SerializeField] int price;
-    public int Price => price;
-
-    [SerializeField] Type itemType;
-    public Type ItemType => itemType;
+    [field: SerializeField] public string ItemName { get; private set; }
+    [field: SerializeField] public Sprite ItemImage { get; private set; }
+    [field: SerializeField] public int StackCount { get; private set; }
+    [field: SerializeField] public int Price { get; private set; }
+    [field: SerializeField] public Type ItemType { get; private set; }
 }
